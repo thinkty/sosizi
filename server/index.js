f149392ce588fe0caddb3249d284465137a0f025
thinkty <thinkty@naver.com>
2022-02-18 13:10:43 +0900

    Changed back from ts to js to see if it works with heroku

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
-import path = require('path');
-import express = require('express');
+const path = require('path');
+const express = require('express');
 const app = express();
 const port = process.env.PORT || 9000;
 const publicPath = path.join(__dirname, '..', '/dist');
